0df932fce53157d14ca35427c075289b845ae1c9
saifsiddqui <30721062+saifsiddqui@users.noreply.github.com>
2021-11-26 22:29:32 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
-function App() {
+export const App = () => {
+  const [height, setHeight ] = useState("");
+  const [weight, setWeight ] = useState("");
+  const [show, setShow] = useState("");
+  const [condition, setCondition] = useState("");
+
+
+  const inputEvent1 = (event) =>{
+    setHeight(event.target.value);
+  }
+  const inputEvent2 = (event) =>{
+    setWeight(event.target.value);
+  }
+  const submit = () => {
+    let height_inm = (height*0.01)*(height*0.01) ;
+    setShow(weight/height_inm);
+
+    if(show < 18.5){
+      setCondition("You are in underweight range");
+    }else if(show > 18.4 && show < 25.0){
+      setCondition("You are in healthy weight range");
+    }else if(show > 24.9 && show < 30.0 ){
+      setCondition("You are in overweight range");
+    }else{
+      setCondition("Obesity class 1")
+    }
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="main">
+      <div className="box">
 
-export default App;
+        <div className="header" style={{
+          backgroundColor: "#1565C0", color: "white", paddingTop: "13px",
+          paddingBottom: "13px"
+        }}>
+          <h1>BMI Calculator</h1>
+        </div>
+
+        <div className="height" style={{marginTop:"20px"}}>
+          <p>Enter your height in cm:</p>
+          <input type="text" onChange={inputEvent1} value={height}/>
+        </div>
+        <div className="weight" style={{marginTop:"20px"}}>
+          <p>Enter your weight in kg:</p>
+          <input type="text" onChange={inputEvent2} value={weight}/>
+        </div>
+        <button onClick={submit} style={{marginTop:"20px"}}>Submit</button>
+
+        <div className="content" style={{marginTop:"20px"}}>
+        <p>Your BMI is {show}</p>
+        <p>Your suggested wieght range is between 50 - 67</p>
+        <p>{condition}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
