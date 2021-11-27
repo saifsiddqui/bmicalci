3b44c767b6f412c7a002d38ddb4efc6644b3c99d
saifsiddqui <30721062+saifsiddqui@users.noreply.github.com>
2021-11-27 22:21:31 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,34 +2,46 @@ import React from 'react';
 import { useState } from 'react';
 
 export const App = () => {
-  const [height, setHeight ] = useState("");
-  const [weight, setWeight ] = useState("");
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
   const [show, setShow] = useState("");
   const [condition, setCondition] = useState("");
+  const [suggest, setSuggest] = useState("");
+  const [bmi, setBmi] = useState();
 
 
-  const inputEvent1 = (event) =>{
+  const inputEvent1 = (event) => {
     setHeight(event.target.value);
   }
-  const inputEvent2 = (event) =>{
+  const inputEvent2 = (event) => {
     setWeight(event.target.value);
   }
-  const submit = () => {
-    let height_inm = (height*0.01)*(height*0.01) ;
-    setShow(weight/height_inm);
 
-    if(show < 18.5){
+  const deliver = () => {
+    if (show < 18.5) {
       setCondition("You are in underweight range");
-    }else if(show > 18.4 && show < 25.0){
+    } else if (show > 18.4 && show < 25.0) {
       setCondition("You are in healthy weight range");
-    }else if(show > 24.9 && show < 30.0 ){
+    } else if (show > 24.9 && show < 30.0) {
       setCondition("You are in overweight range");
-    }else{
+    } else {
       setCondition("Obesity class 1")
     }
   }
+  let height_inm = (height * 0.01) * (height * 0.01);
+  let final = weight / height_inm;
 
-  
+  const submit = () => {
+    if (show === "") {
+      setShow(final);
+      setBmi("Your BMI is ");
+      setSuggest("Your suggested wieght range is between 50 - 67");
+      deliver();
+    }
+
+  }
+
+
   return (
     <div className="main">
       <div className="box">
@@ -41,20 +53,20 @@ export const App = () => {
           <h1>BMI Calculator</h1>
         </div>
 
-        <div className="height" style={{marginTop:"20px"}}>
+        <div className="height" style={{ marginTop: "20px" }}>
           <p>Enter your height in cm:</p>
-          <input type="text" onChange={inputEvent1} value={height}/>
+          <input type="text" onChange={inputEvent1} value={height} />
         </div>
-        <div className="weight" style={{marginTop:"20px"}}>
+        <div className="weight" style={{ marginTop: "20px" }}>
           <p>Enter your weight in kg:</p>
-          <input type="text" onChange={inputEvent2} value={weight}/>
+          <input type="text" onChange={inputEvent2} value={weight} />
         </div>
-        <button onClick={submit} style={{marginTop:"20px"}}>Submit</button>
+        <button onClick={submit} style={{ marginTop: "20px" }}>Submit</button>
 
-        <div className="content" style={{marginTop:"20px"}}>
-        <p>Your BMI is {show}</p>
-        <p>Your suggested wieght range is between 50 - 67</p>
-        <p>{condition}</p>
+        <div className="content" style={{ marginTop: "20px" }}>
+          <p style={{ marginTop: "10px" }}>{bmi}{show}</p>
+          <p style={{ marginTop: "10px" }}>{suggest}</p>
+          <p style={{ marginTop: "10px" }}>{condition}</p>
         </div>
       </div>
     </div>
